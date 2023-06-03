a73621661e7be54a67cb365be545d68a66211555
Ox Noj <geneveva@gmail.com>
2023-06-02 22:40:57 -0700

    Heroku deployment issues

--- server/server.js
+++ server/server.js
@@ -32,10 +32,14 @@ const startApolloServer = async () => {
   await server.start();
   server.applyMiddleware({ app });
 
-  db.once('open', () => {
-    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  })
+  db.once("open", () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
 };
 
 startApolloServer();
