40baf809e54099cca897eaec81d02d858013a8e9
Ox Noj <geneveva@gmail.com>
2023-06-02 17:44:41 -0700

    edited resolvers file - query/mutation hopefully

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,15 +1,65 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
+const { GraphQLError } = require('graphql')
 
 const resolvers = {
     Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const foundUser = User({ _id: context.user_id }).populate('savedBooks');
+                return foundUser
+            }
+            throw AuthenticationError('You are not logged in. Log in to continue');
+        },
+    },
 
-    }
-},
+    Mutation: {
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+            return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
 
-Mutation: {
+            if (!user) {
+                throw new AuthenticationError('There is no user with this email');
+            }
 
-},
+            const correctPW = await user.isCorrectPassword(password);
+
+            if (!correctPW) {
+                throw new AuthenticationError('Wrong password');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        saveBook: async (parent, args, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user_id },
+                    { $addToSet: { savedBooks: args.input } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You are not logged in. Log in to continue');
+        },
+        removeBook: async (parent, { bookId }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findByIdAndUpdate(
+                    { _id: context.user_id },
+                    { $pull: { savedBooks: { bookId } } },
+                    { new: true }
+                );
+                return updatedUser;
+            }
+            throw new AuthenticationError('You are not logged in. Log in to continue');
+        },
+    },
+
+};
 
 module.exports = resolvers;
